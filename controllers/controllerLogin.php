a4eab8745cb713ae5630a31b6fea8d3547760f1c
Gustavo Rodrigues <[gustavorodrigues.pr@outlook.com]>
2020-01-28 20:45:46 -0300

    Finalização do diagrama de classes e atualização do horário do banco

--- controllers/controllerLogin.php
+++ controllers/controllerLogin.php
@@ -5,5 +5,3 @@ $validate->validateEmail($email);
 $validate->validateIssetEmail($email,"login");
 $validate->validateSenha($email,$senha);
 echo $validate->validateFinalLogin($email);
-//var_dump($_SESSION);
-//echo "<script>window.location.href='".DIRPAGE."areaRestrita';</script>";
