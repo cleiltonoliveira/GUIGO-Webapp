19045f0fee2c10a3e0799bf4dc8d977550a8627e
a3ch <43ch@protonmail.ch>
2020-02-10 13:28:51 -0300

    Update link in navbar, and create button Parada Total

--- views/perfil.php
+++ views/perfil.php
@@ -8,7 +8,7 @@ echo $html;
 ?>
 
 <div class="flex-dashboard">
-    <sidebar id="sidebar">
+<sidebar id="sidebar">
         <div class="sidebar-title">
             <img src="../img/Desktop_Guigó_400px.png">
             <h2>GUIGÓ HYDROPONICS</h2>
@@ -17,7 +17,7 @@ echo $html;
             <ul>
                 <li>
                     <img src="../img/Desktop_Guigó_400px.png">
-                    <a href="<?php echo DIRPAGE.'views/perfil'; ?>">PERFIL</a>
+                    <a href="<?php echo DIRPAGE.'perfil'; ?>">PERFIL</a>
                 </li>
                 <li>
                     <img src="../img/CE.png">
@@ -25,27 +25,30 @@ echo $html;
                 </li>
                 <li>
                     <img src="../img/temp_50per.png">
-                    <a href="<?php echo DIRPAGE.'views/temperatura'; ?>">TEMPERATURA</a>
+                    <a href="<?php echo DIRPAGE.'temperatura'; ?>">TEMPERATURA</a>
                 </li>
                 <li>
                     <img src="../img/Humidade_50per.png">
-                    <a href="<?php echo DIRPAGE.'views/umidade'; ?>">UMIDADE</a>
+                    <a href="<?php echo DIRPAGE.'umidade'; ?>">UMIDADE</a>
                 </li>
                 <li>
                     <img src="../img/Luminosidade_50per.png">
-                    <a href="<?php echo DIRPAGE.'views/luminosidade'; ?>">LUMINOSIDADE</a>
+                    <a href="<?php echo DIRPAGE.'luminosidade'; ?>">LUMINOSIDADE</a>
                 </li>
                 <li>
                     <img src="../img/ph_50per.png">
-                    <a href="<?php echo DIRPAGE.'views/ph'; ?>">PH</a>
+                    <a href="<?php echo DIRPAGE.'ph'; ?>">PH</a>
                 </li>
                 <li>
                     <img src="../img/Os2_50per.png">
-                    <a href="<?php echo DIRPAGE.'views/o2'; ?>">O2</a>
+                    <a href="<?php echo DIRPAGE.'o2'; ?>">O2</a>
                 </li>
                 <li>
                     <img src="../img/CE_50per.png">
-                    <a href="<?php echo DIRPAGE.'views/ce'; ?>">CE</a>
+                    <a href="<?php echo DIRPAGE.'ce'; ?>">CE</a>
+                </li>
+                <li>
+                    <button style="color: white; background-color:red;" id="parada-total">PARADA TOTAL</button>
                 </li>
                 <li class="sidebar_logout">
                     <img src="../img/seta_voltar.png">
